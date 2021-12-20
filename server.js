7e3602286279aecfffb8f613fdd4e2cc41ffe2c2
Ryam Sousa <ryam.sousa123@gmail.com>
2021-12-19 22:32:43 -0300

    Heroku configs

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.port || 8080;
+
+app.use(express.static(__dirname + '/dist/my-site-angular'));
+
+app.get('/*', (req, res) => {
+  res.sendFile(__dirname + '/dist/my-site-angular/index.html');
+})
+
+app.listen(PORT, () => {
+  console.log('Server run. Port: '+ PORT);
+})
